b3c2c7ea4aae017c6d376e7a93bc0a49ffb8a534
Aubrey Rhodes <aubrey.c.rhodes@gmail.com>
2013-07-26 13:37:39 -0400

    User can set frequency of task recurrence

--- app/assets/javascripts/views/recurrence.js
+++ app/assets/javascripts/views/recurrence.js
@@ -12,6 +12,11 @@ Keroauc.Views.RecurrenceView = Backbone.View.extend({
   },
   saveRecurrence: function(event){
     event.preventDefault();
+    this.bindFrequency();
     this.model.save();
+  },
+  bindFrequency: function(){
+    var frequency = this.$('input[name=recurrence_frequency]:checked').val();
+    this.model.set({'frequency' : frequency});
   }
 });
