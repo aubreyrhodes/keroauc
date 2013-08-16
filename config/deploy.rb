8e498bb2103929d8e91520505dc33d615405e95e
Aubrey Rhodes <aubrey.c.rhodes@gmail.com>
2013-08-16 16:16:51 -0400

    Add production db adapter

---
+++ config/deploy.rb
@@ -0,0 +1,19 @@
+require "bundler/capistrano"
+
+set :application, "tasks"
+set :repository,  "git@github.com:aubreyrhodes/keroauc.git"
+
+role :web, "dev.acrhodes.com"                          # Your HTTP server, Apache/etc
+role :app, "dev.acrhodes.com"                          # This may be the same as your `Web` server
+role :db,  "dev.acrhodes.com", :primary => true # This is where Rails migrations will run
+
+set :branch, "master"
+set :deploy_via, :remote_cache
+set :user, "deployer"
+set :deploy_to, "/apps/tasks"
+set :use_sudo, false
+set :normalize_asset_timestamps, false
+ssh_options[:forward_agent] = true
+default_run_options[:pty] = true
+
+default_run_options[:shell] = '/bin/zsh -i'
